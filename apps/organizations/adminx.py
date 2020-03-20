e2a0f128c28a4145ed534b2f2d87f289e1ef317d
WeiShuaicc <1538081244@qq.com>
2020-03-20 13:00:59 +0800

    xadmin管理

---
+++ apps/organizations/adminx.py
@@ -0,0 +1,34 @@
+import xadmin
+
+from apps.organizations.models import Teacher, CourseOrg, City
+
+#list_display 定义后台页面显示的字段， Teacher, TeacherAdmin对应
+#search_fields 定义搜索内容
+#list_filter 定义过滤的字段
+#style_fields
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company']
+    search_fields = ['org', 'name', 'work_years', 'work_company']
+    list_filter = ['org', 'name', 'work_years', 'work_company']
+
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums']
+    #修改
+    # style_fields = {
+    #     "desc": "ueditor"
+    # }
+
+
+class CityAdmin(object):
+    list_display = ["id", "name", "desc"]
+    search_fields = ["name", "desc"]
+    list_filter = ["name", "desc", "add_time"]
+    list_editable = ["name", "desc"]
+
+
+xadmin.site.register(Teacher, TeacherAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(City, CityAdmin)
