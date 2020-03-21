52df79a80b1587c8b72ce2e364465fc7befffe6c
WeiShuaicc <1538081244@qq.com>
2020-03-21 23:32:19 +0800

    redis 数据库设置

--- tools/redis_test.py
+++ tools/redis_test.py
@@ -1,9 +1,10 @@
 import redis
+#redis数据库 decode_responses默认转换
 
 r = redis.Redis(host='localhost', port=6379, db=0, charset="utf8", decode_responses=True )
 
 r.set("mobile", "123")
-r.expire("mobile", 1)
+r.expire("mobile", 1)#过期设置
 import time
 time.sleep(1)
 print(r.get("mobile"))
